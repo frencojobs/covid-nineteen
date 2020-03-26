73fd65e22e3450fbb2ed9e7c5c6f97de7a666557
Frenco Jobs <frenco.willian@gmail.com>
2020-03-26 16:58:33 +0630

    added meta tags for seo

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -2,6 +2,7 @@
 const CACHE_NAME = "flutter-app-cache";
 const RESOURCES = {
   "/manifest.json": "f715baba44456a6aa04d804cdc1591fe",
+  "/cover.png": "0e43bbfa80dec344fdd51482e151c7ff",
   "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
   "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
   "/main.dart.js": "6a633e27f8db580f6da6ddb707af3e9b",
@@ -18,7 +19,7 @@ const RESOURCES = {
   "/assets/assets/confirmed_cases.png": "0c59da3ca56365598edc23d15b24f433",
   "/assets/assets/deceased_cases.png": "7d3fde9df38b7072ce23891834ffc791",
   "/assets/assets/serious_cases.png": "029130882208bd18dce941fb545a019c",
-  "/index.html": "922b8ced26c64f343d6d25fef54a7482"
+  "/index.html": "ab22271148805dbf6c9df570cff28f2d"
 };
 
 self.addEventListener("activate", function(event) {
